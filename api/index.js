7e81547d85e5e6f7ffd93e16ed0d05ed78f34763
John Doe <johndoe@example.com>
2024-11-20 20:29:51 +0600

    1.0.1

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/about", (req, res) => res.send("Express on Vercel"));
+app.get("/c", (req, res) => res.send("this is c"));
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
